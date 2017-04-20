5c6b01a4efc8c58f0015026adc6bee378e0d323c
suhas2go <suhas2go@gmail.com>
2017-04-20 21:36:18 +0530

    Add recently watched list

--- url_prior.py
+++ url_prior.py
@@ -1,3 +1,5 @@
+import json
+import os
 import sqlite3
 from database_setup.cosine_sim import cosine_sim
 
@@ -12,3 +14,20 @@ def url_prior(user_id, document):
         sum += (cosine_sim(document,row[1]))
     normalized_score = sum/i
     return normalized_score
+
+PATH = "dataset/"
+def get_recently_watched(user_id):
+    conn = sqlite3.connect('database_setup/history.db')
+    cursor = conn.execute("SELECT VIDEO_ID FROM CLICK_THROUGH WHERE USER_ID=?", (user_id,))
+    recently_watched = []
+    filelist = os.listdir(PATH)
+    for i in range(len(filelist)):
+        filelist[i] = PATH + filelist[i]
+        page = open(filelist[i], "r")
+        parsed = json.loads(page.read())
+        for r in cursor:
+            if r[0]==parsed['videoInfo']['id']:
+                recently_watched.append(parsed)
+                break
+
+    return recently_watched
